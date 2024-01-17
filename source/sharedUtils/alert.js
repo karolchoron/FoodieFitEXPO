5c246c606becdd8230d66840196d543170d10e2b
karolchoron <karolchoron@gmail.com>
2024-01-17 14:10:47 +0100

    alert web fix

--- source/sharedUtils/alert.js
+++ source/sharedUtils/alert.js
@@ -1,25 +1,30 @@
 import { Alert, Platform } from 'react-native';
 
-const alertPolyfill = (title, description, options = []) => {
-    const result = window.confirm([title, description].filter(Boolean).join('\n'));
-
-    if (result) {
-        // Znajdz i wykonaj funkcje onPress dla pierwszej opcji, ktora nie jest typu 'cancel'
-        const confirmOption = options.find(({ style }) => style !== 'cancel');
-        if (confirmOption && confirmOption.onPress) {
-            confirmOption.onPress();
-        }
-    } else {
-        // Znajdz i wykonaj funkcje onPress dla opcji 'cancel'
-        const cancelOption = options.find(({ style }) => style === 'cancel');
-        if (cancelOption && cancelOption.onPress) {
-            cancelOption.onPress();
-        }
-    }
-};
-
-// komponent react native alert domyslnie nie dziala na Web.
+// komponent react native alert domyslnie nie dziala w Webowej apce.
 // Uzyj alertPolyfill na platformie webowej, a domyslny Alert na platformach mobilnych
-const alert = Platform.OS === 'web' ? alertPolyfill : Alert.alert;
+let alert;
+
+if (Platform.OS === 'web') {
+    const alertPolyfill = (title, description, options = []) => {
+        const result = window.confirm([title, description].filter(Boolean).join('\n'));
+
+        if (result) {
+            // Znajdz i wykonaj funkcje onPress dla pierwszej opcji, ktora nie jest typu 'cancel'
+            const confirmOption = options.find(({ style }) => style !== 'cancel');
+            if (confirmOption && confirmOption.onPress) {
+                confirmOption.onPress();
+            }
+        } else {
+            // Znajdz i wykonaj funkcje onPress dla opcji 'cancel'
+            const cancelOption = options.find(({ style }) => style === 'cancel');
+            if (cancelOption && cancelOption.onPress) {
+                cancelOption.onPress();
+            }
+        }
+    };
+    alert = alertPolyfill;
+} else {
+    alert = Alert.alert;
+}
 
 export default alert;
