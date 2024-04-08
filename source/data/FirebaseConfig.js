c8f7d45dd9800d60c5b73db23bc2ff3fe76050d7
karolchoron <karolchoron@gmail.com>
2024-04-08 23:08:01 +0200

    database info

--- source/data/FirebaseConfig.js
+++ source/data/FirebaseConfig.js
@@ -2,16 +2,16 @@ import { getDatabase } from 'firebase/database';
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
-// Konfiguracja poswiadczen do bazy Firebase
+// Konfiguracja poswiadczen do bazy Firebase (projekt usuniety)
 const firebaseConfig = {
-  apiKey: "AIzaSyDHZKDh7o6KBQXpd1XmJHPQov9B0THIj2c",
-  authDomain: "foodiefit-2f8e3.firebaseapp.com",
-  projectId: "foodiefit-2f8e3",
-  storageBucket: "foodiefit-2f8e3.appspot.com",
-  messagingSenderId: "811770449362",
-  appId: "1:811770449362:web:c34b7fb25aaf8572eecc0b",
-  measurementId: "G-VD9K68JK5H",
-  databaseURL: "https://foodiefit-2f8e3-default-rtdb.europe-west1.firebasedatabase.app",
+  apiKey: "XXX",
+  authDomain: "XXX",
+  projectId: "XXX",
+  storageBucket: "XXX",
+  messagingSenderId: "XXX",
+  appId: "XXX",
+  measurementId: "XXX",
+  databaseURL: "XXX",
 };
 
 // Inicializacja Firebase
@@ -23,3 +23,63 @@ const FIREBASE_AUTH = getAuth();
 const FIREBASE_DATABASE = getDatabase();
 
 export { FIREBASE_APP, FIREBASE_DATABASE, FIREBASE_AUTH }
+
+// INFORMACJA BAZA DANYCH
+// STRUKTURA BAZY DANYCH NOSQL FIREBASE REALTIMEDATABSE:
+// + FIREBASE AUTHENTICATION
+
+// /dishes/classic/breakfast
+// /dishes/classic/dinner
+// /dishes/classic/supper
+// /dishes/wegetarian/breakfast
+// /dishes/wegetarian/dinner
+// /dishes/wegetarian/supper
+// /users/id/caloriesQuantity
+// /users/id/id
+// /users/id/dateOfGenerateTheDayDish
+// /users/id/idOfGeneratedTheDayDish
+// /users/id/lastName
+// /users/id/name
+// /users/id/preferedTypeOfDiet
+
+// PRZYKLAD:
+// {
+//   "dishes": {
+//     "classic": {
+//       "breakfast": [
+//         null,
+//         {
+//           "id": 1,
+//           "name": "Kanapki z Serem i Szynką",
+//           "products": {
+//             "bulka": {
+//               "calories": 300,
+//               "name": "Bułka",
+//               "weight": 100
+//             },
+//             "ser": {
+//               "calories": 200,
+//               "name": "Ser",
+//               "quantity": 2
+//             },
+//             "szynka": {
+//               "calories": 150,
+//               "name": "Szynka",
+//               "quantity": 2
+//             }
+//           },
+//           "totalCalories": 650
+//         },
+// 		[....]
+		
+// "users": {
+//     "xxxxxxardeasdwqasdasdsa": {
+//       "caloriesQuantity": 0,
+//       "dateOfGenerateTheDayDish": "",
+//       "id": "ur9HxuyuhNYVzdaYhRDI3ib1yIv1",
+//       "idOfGeneratedTheDayDish": "",
+//       "lastName": "Kowalski",
+//       "name": "Jan",
+//       "preferedTypeOfDiet": "Klasyczna"
+//     },
+// 	[...]
